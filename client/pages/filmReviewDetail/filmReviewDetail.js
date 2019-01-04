e07f94d1e14532c446c553d1519d515d3a4c0115
yujiyuan <907392863@qq.com>
2019-01-04 11:25:25 +0800

    已完成所有流程

--- client/pages/filmReviewDetail/filmReviewDetail.js
+++ client/pages/filmReviewDetail/filmReviewDetail.js
@@ -11,115 +11,119 @@ Page({
    */
   data: {
     filmDetail: null,
-    reviewDetail: null,
+    reviewDetail: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    const that = this
+    const that = this;
     util.getFilmDetail({
       // 设置电影详情
       id: options.id,
       success: ({ filmDetail }) => {
-        that.setData({ filmDetail })
+        that.setData({ filmDetail });
       }
-    })
-    console.log('options', options)
+    });
+    console.log("options", options);
 
-    this.getReviewDetail(options.review_id)
+    this.getReviewDetail(options.review_id, options.id, options.isIndexGetInto);
   },
   /**
    * 播放录音
    */
   onPlay() {
-    innerAudioContext.src = this.data.reviewDetail.tempFilePath
+    innerAudioContext.src = this.data.reviewDetail.tempFilePath;
     innerAudioContext.play(() => {
-      console.log('开始播放')
-    })
+      console.log("开始播放");
+    });
     innerAudioContext.onError(res => {
-      console.log(res.errMsg)
-      console.log(res.errCode)
-    })
+      console.log(res.errMsg);
+      console.log(res.errCode);
+    });
   },
   /**
    *获取影评详情
    */
-  getReviewDetail(review_id) {
-    wx.showLoading({ title: '获取影评详情中' })
+  getReviewDetail(review_id, id, isIndexGetInto) {
+    wx.showLoading({ title: "获取影评详情中" });
+    console.log(isIndexGetInto);
+    const data =
+      isIndexGetInto === "false"
+        ? { id, isIndexGetInto: true }
+        : { review_id, isIndexGetInto: false };
     qcloud.request({
       url: config.service.getReviewDetail,
       login: true,
-      method: 'GET',
-      data: {
-        review_id
-      },
+      method: "GET",
+      data: data,
       success: response => {
-        const { data } = response.data
-        console.log(data)
-        this.setData({ reviewDetail: data[0] })
+        const { data } = response.data;
+        console.log(data);
+        this.setData({ reviewDetail: data[0] });
       },
       fail: err => {
-        console.log(err)
+        console.log(err);
       },
       complete: () => {
-        wx.hideLoading()
+        wx.hideLoading();
       }
-    })
+    });
   },
   /**
    * 点击进入编辑影评页面
    */
   onTapWrite(event) {
-    const { id } = event.currentTarget.dataset
+    const { id } = event.currentTarget.dataset;
 
     wx.showActionSheet({
-      itemList: ['文字', '音频'],
+      itemList: ["文字", "音频"],
       success(res) {
-        console.log(res.tapIndex)
+        console.log(res.tapIndex);
         wx.navigateTo({
           url: `/pages/editFilmReview/editFilmReview?id=${id}&tapIndex=${
             res.tapIndex
           }`
-        })
+        });
       },
       fail(res) {
-        console.log(res.errMsg)
+        console.log(res.errMsg);
       }
-    })
+    });
   },
   /**
    * 收藏影评
    */
-  onTapCollection(){
-    wx.showLoading({ title: '收藏中' })
+  onTapCollection() {
+    wx.showLoading({ title: "收藏中" });
     const { filmDetail, reviewDetail } = this.data;
-    const { id, title, image } = filmDetail
-    const { content, tempFilePath, duration } = reviewDetail
-    console.log(filmDetail, reviewDetail)
+    const { id, title, image } = filmDetail;
+    const { content, tempFilePath, duration } = reviewDetail;
+    console.log(filmDetail, reviewDetail);
     qcloud.request({
       url: config.service.addCollectionReviews,
       login: true,
-      method: 'POST',
+      method: "POST",
       data: {
         movie_id: id,
         title,
         image,
         content,
-        tempFilePath, duration
+        tempFilePath,
+        duration
       },
       success: response => {
-        const { data } = response.data
-        console.log(data)
+        const { data } = response.data;
+        console.log(data);
         // this.setData({ reviewDetail: data[0] })
       },
       fail: err => {
-        console.log(err)
+        console.log(err);
       },
       complete: () => {
-        wx.hideLoading()
+        wx.hideLoading();
       }
-    })
+    });
   }
-})
+});
--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,92 +1,104 @@
 // pages/recommend/recommend.js
-const qcloud = require('../../vendor/wafer2-client-sdk/index');
+const app = getApp();
+const qcloud = require("../../vendor/wafer2-client-sdk/index");
 const config = require("../../config.js");
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    hotFilm:{}, //热门电影
-    randomViewFilm:{}//随机热门电影 
+    userInfo: {},
+    hotFilm: {}, //热门电影
+    randomViewFilm: {} //随机热门电影
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    wx.showLoading({ title: "正在登录中" });
+    app.checkSession({
+      // 获取用户信息
+      success: ({ userInfo }) => {
+        wx.hideLoading();
+        this.setData({ userInfo });
+      }
+    });
     this.getRandomHotFilm();
   },
   // 刷新页面
   onPullDownRefresh() {
-    this.getRandomHotFilm(() => wx.stopPullDownRefresh())
+    this.getRandomHotFilm(() => wx.stopPullDownRefresh());
   },
   /**
    * 随机获取热门电影的影评
    */
-  getRandomHotFilm(callback){
+  getRandomHotFilm(callback) {
     wx.showLoading({
-      title: '电影数据加载中'
-    })
+      title: "电影数据加载中"
+    });
     qcloud.request({
       url: config.service.getFilmList,
-      success: result=>{
-        const {data} = result;
-        
-        if(!data.code) {
+      success: result => {
+        const { data } = result;
+
+        if (!data.code) {
           const filmList = data.data;
-          const randomViewFilm = filmList[Math.floor(Math.random() * filmList.length)]
-          console.log('item', randomViewFilm)
+          const randomViewFilm =
+            filmList[Math.floor(Math.random() * filmList.length)];
+          console.log("item", randomViewFilm);
           this.setData({
             randomViewFilm
-          })
-        }else {
+          });
+        } else {
           wx.showToast({
-            title:"数据加载失败！",
-            icon:"none"
-          })
+            title: "数据加载失败！",
+            icon: "none"
+          });
         }
       },
       fail: () => {
         wx.showToast({
-          title: '数据加载失败',
-          icon: 'none'
-        })
+          title: "数据加载失败",
+          icon: "none"
+        });
       },
       complete: () => {
-        wx.hideLoading()
-        callback && callback()
+        wx.hideLoading();
+        callback && callback();
       }
-    })
+    });
   },
   /**
    * 点击跳转到热门页面
    */
   onTapToPopular() {
     wx.navigateTo({
-      url: '/pages/filmList/filmList'
-    })
+      url: "/pages/filmList/filmList"
+    });
   },
   /**
    * 点击跳转到我的页面
    */
   onTapToUser() {
     wx.navigateTo({
-      url: '/pages/user/user'
-    })
+      url: "/pages/user/user"
+    });
   },
   /**
    * 点击跳转到详情页
    */
   onTapToDetail(event) {
-    const {id} = event.currentTarget.dataset;
-    wx.navigateTo({ url: `/pages/detail/detail?id=${id}` })
+    const { id } = event.currentTarget.dataset;
+    wx.navigateTo({ url: `/pages/detail/detail?id=${id}` });
   },
   /**
    * 点击跳转到影评详情页
    */
-  onTapToFilmReviewDetail() {
+  onTapToFilmReviewDetail(event) {
+    const { id } = event.currentTarget.dataset;
     wx.navigateTo({
-      url: '/pages/filmReviewDetail/filmReviewDetail'
-    })
+      url: `/pages/filmReviewDetail/filmReviewDetail?id=${id}&isIndexGetInto=false`
+    });
   }
-})
+});
